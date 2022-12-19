a1f71eb76c01cabcb1f84258433db2e9fda7962b
Diogorrib <119450853+Diogorrib@users.noreply.github.com>
2022-12-19 00:26:59 +0000

    Add files via upload

---
+++ tests/1_2_test.c
@@ -0,0 +1,13 @@
+#include "fs/operations.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    printf("Successful test.\n");
+
+    return 0;
+}
